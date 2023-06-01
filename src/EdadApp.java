c7eacf532888acd65b603d39f7413b99f6dc67ac
IsmaelPatón <ispacreas@gmail.com>
2023-06-01 11:08:30 +0200

    trabajo terminado

--- src/EdadApp.java
+++ src/EdadApp.java
@@ -1,4 +1,31 @@
 
 public class EdadApp {
 
+	private int[] array;
+
+    public EdadApp(int[] array) {
+        this.array = array;
+    }
+   
+    public String realizarSuma() {
+    	int mayor = 0;
+    	int menor = 0;
+    	int jubilado = 0;
+    	int i ;
+     for (i=0 ; i<array.length; i++) {
+    	 if (array[i]<18){
+    		menor = menor+1; 
+    	 }
+    	 if (array[i]>=18) {
+    		 mayor = mayor+1;
+    	 }
+    	 if (array[i]>=65) {
+    		 jubilado = jubilado+1;
+    	 }
+     }
+    String cadena= "***Menores de edad:" + menor+ " ***Mayores de edad:" +mayor+" ***Jubilados :" +jubilado+"***";
+    	return cadena;
+    	
+    }
+      
 }
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -2,8 +2,11 @@
 public class PrincipalApp {
 
 	public static void main(String[] args) {
-		
-
+		int [] array = {45,55, 66, 17, 18, 23, 5, 7, 75,87};
+        EdadApp realizarsuma = new EdadApp(array);
+        String resultado = realizarsuma.realizarSuma();
+        System.out.println(resultado);
+        
 	}
 
 }
